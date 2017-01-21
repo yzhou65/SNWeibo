bdf8806f9e6e529f12504d434fe084219e2bbf4c
Yue <yue@Yues-MacBook-Pro.local>
2017-01-20 22:57:19 -0600

    project init

---
+++ SNWeibo/Classes/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  SNWeibo
+//
+//  Created by Yue Zhou on 1/20/17.
+//  Copyright © 2017 Yue Zhou. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
